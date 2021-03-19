673354bb18fdfd68cfa23bb5684a78992bcb1cff
Alena Filatova <ppasd2005@yandex.ru>
2021-03-19 21:21:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int count1 = 2_000_000_000;
+       int count2 = 500_000_000;
+       int total = count1 + count2;
+        System.out.println(total);
+    }
+}
